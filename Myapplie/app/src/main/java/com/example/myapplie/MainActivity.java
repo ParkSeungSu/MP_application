d047f38921c566e5510b8eb99c7c264fe2587e3e
ParkSeungSu <cldkr0401@naver.com>
2019-09-03 15:35:57 +0900

    ..

---
+++ Myapplie/app/src/main/java/com/example/myapplie/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView tv = findViewById(R.id.tv);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+                if(tv.getText()!="hello"){
+                tv.setText("hello");}
+                else{
+                    tv.setText("똵");
+                }
+            }
+        });
+
+    }
+}
